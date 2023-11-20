2e8fd2d3501d7edb2d01c3d5b836bf234a0ae6c8
Sorin Turda <s.turda12@gmail.com>
2023-11-20 12:21:09 +0200

    lab 06 complet

--- 06/main.cpp
+++ 06/main.cpp
@@ -1,6 +1,10 @@
+/*
+ * Sorin Turda, grupa 30222
+ * pentru reprezentarea R2 am folosit un vector care foloseste O(n) memorie aditionala
+ * astfel constructia T1 s-a facut in timp liniar
+ * pentru constructia arborelui T2 constructia s-a facut tot in timp liniar
+ */
 #include <iostream>
-#include <stdio.h>
-#include <time.h>
 #include <string.h>
 #include <unordered_map>
 #include <vector>
@@ -17,8 +21,27 @@ struct nod_b {
     nod_b *frate;
 };
 
+nod *creare(int k, int n) {
+    nod *p = new nod;
+    p->k = k;
+    p->marime = n;
+    p->copii = (nod**)malloc(n * sizeof(nod*));
+    return p;
+}
+
+nod_b *creare(int k) {
+    nod_b *p = new nod_b;
+    p->k = k;
+    p->copil = NULL;
+    p->frate = NULL;
+    return p;
+}
+
 void afisare_prietenoasa(int p[], int n, int parinte, int spatii) {
     //pentru R1
+    //o implementare mai eficienta pentru functie
+    //foloseste vectorul cu copii si astfel 
+    //nu se mai parcurge tot vectorul de parinti
     char s[spatii + 1];
     memset(s, ' ', spatii);
     s[spatii] = '\0';
@@ -41,31 +64,16 @@ void afisare_prietenoasa(nod *radacina, int spatii) {
     }
 }
 
-// void afisare_prietenoasa(nod_b *radacina, int spatii) {
-//     //pentru R3
-//     char s[spatii + 1];
-//     memset(s, ' ', spatii);
-//     s[spatii] = '\0';
-//     printf("%s%d\n", s, radacina->k);
-//     for (int i = 0; i < radacina->marime; i++) {
-//         afisare_prietenoasa(radacina->copii[i], spatii + 4);
-//     }
-// }
-
-nod *creare(int k, int n) {
-    nod *p = new nod;
-    p->k = k;
-    p->marime = n;
-    p->copii = (nod**)malloc(n * sizeof(nod*));
-    return p;
-}
-
-nod_b *creare(int k) {
-    nod_b *p = new nod_b;
-    p->k = k;
-    p->copil = NULL;
-    p->frate = NULL;
-    return p;
+void afisare_prietenoasa(nod_b *radacina, int spatii) {
+    //pentru R3
+    if (radacina == NULL)
+        return;
+    char s[spatii + 1];
+    memset(s, ' ', spatii);
+    s[spatii] = '\0';
+    printf("%s%d\n", s, radacina->k);
+    afisare_prietenoasa(radacina->copil, spatii + 4);
+    afisare_prietenoasa(radacina->frate, spatii);
 }
 
 nod *construire(std::vector<std::vector<int>> &a, int parinte) {
@@ -81,9 +89,10 @@ nod_b *construire(nod *radacina_veche) {
     nod_b *radacina = creare(radacina_veche->k);
     if (radacina_veche->marime) {
         radacina->copil = construire(radacina_veche->copii[0]);
-        for (int i = 1; i < radacina_veche->marime; i++) {
-            radacina->frate = construire(radacina_veche->copii[i]);
-            radacina = radacina->frate;
+        nod_b *p = radacina->copil;
+        for (int i = 1; p != NULL && i < radacina_veche->marime; i++) {
+            p->frate = construire(radacina_veche->copii[i]);
+            p = p->frate;
         }
     }
     return radacina;
@@ -91,7 +100,6 @@ nod_b *construire(nod *radacina_veche) {
 
 void demo() {
     int p[] = {2, 7, 5, 2, 7, 7, -1, 5, 2};
-    // int p[] = { -1, 1, 2, 3, 4, 5, 6, 7};
     int n = sizeof(p) / sizeof(p[0]);
     int radacina_;
     std::vector<std::vector<int>>a(n, std::vector<int>(0));
@@ -102,10 +110,11 @@ void demo() {
             radacina_ = i + 1;
     nod *radacina = construire(a, radacina_);
     nod_b *radacina_noua = construire(radacina);
-    // afisare_prietenoasa(p, n, -1, 0);
-    // puts("");
-    // afisare_prietenoasa(radacina, 0);
-    std::cout<<radacina_noua->copil->copil->frate->k;
+    afisare_prietenoasa(p, n, -1, 0);
+    puts("");
+    afisare_prietenoasa(radacina, 0);
+    puts("");
+    afisare_prietenoasa(radacina_noua, 0);
 }
 
 int main() {
