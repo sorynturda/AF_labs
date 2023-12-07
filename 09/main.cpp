22983e85bfd97b12fa15d718fe608ddd317bed83
Sorin Turda <s.turda12@gmail.com>
2023-12-07 15:17:04 +0200

    lab 09 graf conex aleator

--- 09/main.cpp
+++ 09/main.cpp
@@ -2,7 +2,12 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <set>
 #include "../Profiler.h"
+
+#define MAX_SIZE 10000
+#define STEP_SIZE 100
+#define NR_TESTS 5
 
 struct NodeT {
     int key;
@@ -71,20 +76,39 @@ void unionn(NodeT *&x, NodeT *&y) {
     link(set1, set2);
 }
 
-edge *kruskal(edge edges[], int n) {
+edge *kruskal(edge edges[], int n, int &m) {
     int count = 0;
-    NodeT *set[n / 4];
-    edge *ans = (edge*)malloc((n / 4 - 1) * sizeof(edge));
-    for (int i = 0; i < n / 4; i++)
+    NodeT *set[n];
+    edge *res = (edge*)malloc((n - 1) * sizeof(edge));
+    for (int i = 0; i < n; i++)
         set[i] = make_set(i);
-    quicksort(edges, 0, n - 1);
-    for (int i = 0; i < n; i++) {
+    quicksort(edges, 0, m - 1);
+    for (int i = 0; i < m; i++) {
         if (find_set(set[edges[i].u]) != find_set(set[edges[i].v])) {
-            ans[count++] = edges[i];
+            res[count++] = edges[i];
             unionn(set[edges[i].u], set[edges[i].v]);
         }
     }
-    return ans;
+    // if (m == n) {
+    //     std::set<int>S;
+    //     for (int i = 0; i < n; i++)
+    //         S.insert(find_set(set[i])->key);
+    //     for (int i = 0; i < n; i++)
+    //         std::cout << find_set(set[i])->key << ' ';
+    //     m = S.size();
+    // }
+    return res;
+}
+
+edge get_edge(int u, int n) {
+    edge res;
+    res.u = u;
+    int v = rand() % n;
+    while (v == u)
+        v = rand() % n;
+    res.v = v;
+    res.w = rand() % MAX_SIZE;
+    return res;
 }
 
 void demo() {
@@ -105,7 +129,7 @@ void demo() {
 }
 
 void demo_kruskal() {
-    int n = 4;
+    int n = 4, m = n * 4;
     edge edges[] = {
         {0, 1, 5}, {1, 2, 3}, {2, 3, 7}, {3, 0, 4},
         {0, 2, 3}, {0, 3, 1}, {1, 3, 2}, {1, 3, 4},
@@ -113,18 +137,34 @@ void demo_kruskal() {
         {0, 2, 9}, {2, 3, 5}, {1, 3, 4}, {1, 3, 7}
     };
     // quicksort(edges,0,15);
-    // for (int i = 0; i < 4 * n; i++)
+    // for (int i = 0; i < m; i++)
     //     printf("%d %d %d\n", edges[i].u, edges[i].v, edges[i].w);
     // puts("");
-    edge *ans = kruskal(edges, 4 * n);
+    edge *res = kruskal(edges, n, m);
     for (int i = 0; i < n - 1; i++)
-        printf("%d - %d: %d\n", ans[i].u, ans[i].v, ans[i].w);
+        printf("%d - %d: %d\n", res[i].u, res[i].v, res[i].w);
+}
+
+void perf() {
+    edge edges[100];
+    // for (int n = STEP_SIZE; n <= MAX_SIZE; n += STEP_SIZE) {
+    //     for (int u = 0; u < n; u++) {
+    //         edges[i] = get_edge(u, n);
+    //     }
+    // }
+    int m = 100;
+    for (int u = 0; u < 100; u++)
+        edges[u] = get_edge(u, 100);
+    edge *res = kruskal(edges, 100, m);
+    std::cout << '\n' << m << '\n';
 }
 
 int main() {
+    srand(time(nullptr));
     clock_t begin = clock();
     // demo();
-    demo_kruskal();
+    // demo_kruskal();
+    perf();
     clock_t end = clock();
     printf("%lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
     return 0;
