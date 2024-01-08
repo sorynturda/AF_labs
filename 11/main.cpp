bc19ebe6d73763e5d8b6e4b38ee32d667c4999a4
Sorin Turda <s.turda12@gmail.com>
2024-01-08 18:49:44 +0200

    tarjan merge si sortare topo bine

--- 11/main.cpp
+++ 11/main.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <time.h>
 #include <set>
+#include <vector>
 #include <stack>
-#include "../Profiler.h"
+#include "/home/sorin/Documents/facultate/AF_labs/Profiler.h"
 
 #define MAX_SIZE 10000
 #define STEP_SIZE 100
@@ -27,7 +28,6 @@ typedef struct _NodeT {
     struct _NodeT *parent;
 } NodeT;
 
-
 typedef struct _Node {
     int key;
 
@@ -37,6 +37,12 @@ typedef struct _Node {
     int color;
     int d;
     int f;
+
+    bool onStack;
+    int index;
+    int lowLink;
+    int comp;
+
     struct _Node *parent;
 } Node;
 
@@ -45,13 +51,17 @@ typedef struct {
     Node **v;
 } Graph;
 
+void strong_connect(Graph *graph, Node *s);
+
 void dfs(Graph *graph, Node *s, Operation *op = NULL);
 
-std::stack<Node*> stiva;
+std::stack<Node *> stiva;
+
+int INDEX = 0, nrComponents = 0;
 bool cycle = false;
 
 NodeT *make_set(int key) {
-    NodeT *x = (NodeT*)malloc(sizeof(NodeT));
+    NodeT *x = (NodeT *) malloc(sizeof(NodeT));
     x->key = key;
     x->parent = x;
     x->rank = 0;
@@ -64,7 +74,7 @@ NodeT *find_set(NodeT *x) {
     return x->parent;
 }
 
-void link(NodeT **x, NodeT * (*y)) {
+void link(NodeT **x, NodeT *(*y)) {
     if ((*x)->rank > (*y)->rank)
         (*y)->parent = (*x);
 
@@ -100,50 +110,18 @@ void free_graph(Graph *graph) {
     graph->nrNodes = 0;
 }
 
-void fa_graf_conex(Graph *graph, int n_edges) {
-    Point *edges = (Point*)malloc(n_edges * sizeof(Point));
-    bool a[graph->nrNodes][graph->nrNodes] = {false};
-    for (int i = 0; i < graph->nrNodes; i++) {
-        edges[i].row = i;
-        edges[i].col = rand() % graph->nrNodes;
-        while (edges[i].row == edges[i].col && a[edges[i].row][edges[i].col] == true)
-            edges[i].col = rand() % graph->nrNodes;
-        a[edges[i].row][edges[i].col] = true;
-        // a[edges[i].col][edges[i].row] = true;
+void fa_graf(Graph *graph, int n_edges) {
+    bool a[graph->nrNodes][graph->nrNodes];
+    for (int i = 0; i < graph->nrNodes; i++)for (int j = 0; j < graph->nrNodes; j++)a[i][j] = 0;
+    for (int i = 0; i < n_edges; i++) {
+        int u = rand() % graph->nrNodes;
+        int v = rand() % graph->nrNodes;
+        while (u == v || a[u][v])
+            v = rand() % graph->nrNodes;
+        a[u][v] = true;
+        // a[v][u] = true;
     }
     int k;
-
-    NodeT *s[graph->nrNodes];
-    for (int i = 0; i < graph->nrNodes; i++)
-        s[i] = make_set(i);
-    for (int i = 0; i < graph->nrNodes; i++)
-        if (find_set(s[edges[i].row]) != find_set(s[edges[i].col]))
-            unionn(&s[edges[i].row], &s[edges[i].col]);
-
-    std::set<int>S;
-    for (int i = 0; i < graph->nrNodes; i++) {
-        S.insert(find_set(s[i])->key);
-    }
-    int connectedComponents = S.size();
-    for (int i = graph->nrNodes; i < graph->nrNodes + connectedComponents; i++) {
-        int u = *S.begin();
-        S.erase(u);
-        int v = *S.begin();
-        unionn(&s[u], &s[v]);
-        a[u][v] = a[v][u] = true;
-        S.clear();
-        for (int i = 0; i < graph->nrNodes; i++)
-            S.insert(find_set(s[i])->key);
-    }
-
-    for (int i = graph->nrNodes + connectedComponents; i < n_edges; i++) {
-        edges[i].row = rand() % graph->nrNodes;
-        edges[i].col = rand() % graph->nrNodes;
-        while (edges[i].row == edges[i].col && a[edges[i].row][edges[i].col] == true)
-            edges[i].col = rand() % graph->nrNodes;
-        a[edges[i].row][edges[i].col] = true;
-        // a[edges[i].col][edges[i].row] = true;
-    }
     for (int i = 0; i < graph->nrNodes; i++) {
         k = 0;
         // graph->v[i]->key = i;
@@ -151,7 +129,7 @@ void fa_graf_conex(Graph *graph, int n_edges) {
             if (a[i][j])
                 k++;
         graph->v[i]->adjSize = k;
-        graph->v[i]->adj = (Node**)malloc(k * sizeof(Node*));
+        graph->v[i]->adj = (Node **) malloc(k * sizeof(Node *));
     }
     for (int i = 0; i < graph->nrNodes; i++) {
         k = 0;
@@ -159,8 +137,6 @@ void fa_graf_conex(Graph *graph, int n_edges) {
             if (a[i][j])
                 graph->v[i]->adj[k++] = graph->v[j];
     }
-    if (edges != NULL)
-        free(edges), edges = NULL;
 }
 
 void dfs(Graph *graph, Node *s, Operation *op) {
@@ -173,12 +149,15 @@ void dfs(Graph *graph, Node *s, Operation *op) {
     for (int i = 0; i < s->adjSize; i++) {
         if (op != NULL)
             op->count();
-        if(s->adj[i]->d != 0 && s->adj[i]->f != 0 && s->adj[i]->color != COLOR_WHITE)
+        Node *v = s->adj[i];
+        if (v->parent && v->d && v->f) {
             cycle = true;
-        if (s->adj[i]->color == COLOR_WHITE) {
+//            std::cout<<s->key<<' '<<s->adj[i]->key<<'\n';
+        }
+        if (v->color == COLOR_WHITE) {
             if (op != NULL)
                 op->count();
-            s->adj[i]->parent = s;
+            v->parent = s;
             dfs(graph, s->adj[i], op);
         }
     }
@@ -199,14 +178,14 @@ void performance() {
         Graph graph;
         graph.nrNodes = 100;
         //initialize the nodes of the graph
-        graph.v = (Node**)malloc(graph.nrNodes * sizeof(Node*));
+        graph.v = (Node **) malloc(graph.nrNodes * sizeof(Node *));
         for (i = 0; i < graph.nrNodes; ++i) {
-            graph.v[i] = (Node*)malloc(sizeof(Node));
+            graph.v[i] = (Node *) malloc(sizeof(Node));
             memset(graph.v[i], 0, sizeof(Node));
         }
         // TODO: generate n random edges
         // make sure the generated graph is connected
-        fa_graf_conex(&graph, n);
+        fa_graf(&graph, n);
         dfs(&graph, graph.v[0], &op);
         free_graph(&graph);
     }
@@ -217,14 +196,14 @@ void performance() {
         Graph graph;
         graph.nrNodes = n;
         //initialize the nodes of the graph
-        graph.v = (Node**)malloc(graph.nrNodes * sizeof(Node*));
+        graph.v = (Node **) malloc(graph.nrNodes * sizeof(Node *));
         for (i = 0; i < graph.nrNodes; ++i) {
-            graph.v[i] = (Node*)malloc(sizeof(Node));
+            graph.v[i] = (Node *) malloc(sizeof(Node));
             memset(graph.v[i], 0, sizeof(Node));
         }
         // TODO: generate 4500 random edges
         // make sure the generated graph is connected
-        fa_graf_conex(&graph, 4500);
+        fa_graf(&graph, 4500);
         dfs(&graph, graph.v[0], &op);
         free_graph(&graph);
     }
@@ -232,17 +211,85 @@ void performance() {
     p.showReport();
 }
 
+void tarjan(Graph *graph) {
+    for (int i = 0; i < graph->nrNodes; i++) {
+        graph->v[i]->index = graph->v[i]->lowLink = -1;
+        graph->v[i]->onStack = false;
+        graph->v[i]->comp = 0;
+    }
+    for (int i = 0; i < graph->nrNodes; i++)
+        if (graph->v[i]->index == -1)
+            strong_connect(graph, graph->v[i]);
+}
+
+void strong_connect(Graph *graph, Node *s) {
+//     std::cout<<"ADS\n";
+    s->index = s->lowLink = INDEX;
+    INDEX++;
+    stiva.push(s);
+    s->onStack = true;
+    for (int i = 0; i < s->adjSize; i++)
+        if (s->adj[i]->index == -1) {
+            strong_connect(graph, s->adj[i]);
+            s->lowLink = std::min(s->adj[i]->lowLink, s->lowLink);
+        } else if (s->adj[i]->onStack)
+            s->lowLink = std::min(s->lowLink, s->adj[i]->index);
+    if (s->lowLink == s->index) {
+        nrComponents++;
+        Node *u;
+        do {
+            if(stiva.empty())
+                break;
+            u = stiva.top();
+            stiva.pop();
+            u->onStack = false;
+            u->comp = nrComponents;
+        } while (s == u);
+    }
+}
+
+void ddemo() {
+    Graph graph;
+    graph.nrNodes = 5;
+    graph.v = (Node **) malloc(graph.nrNodes * sizeof(Node *));
+    for (int i = 0; i < graph.nrNodes; ++i) {
+        graph.v[i] = (Node *) malloc(sizeof(Node));
+        memset(graph.v[i], 0, sizeof(Node));
+        graph.v[i]->key = i;
+        graph.v[i]->color = COLOR_WHITE;
+        graph.v[i]->f = 0;
+        graph.v[i]->d = 0;
+        graph.v[i]->parent = NULL;
+    }
+    fa_graf(&graph, 6);
+    for (int i = 0; i < graph.nrNodes; i++) {
+        std::cout << i << ": ";
+        for (int j = 0; j < graph.v[i]->adjSize; j++)
+            std::cout << graph.v[i]->adj[j]->key << ' ';
+        std::cout << '\n';
+    }
+    tarjan(&graph);
+    std::cout << nrComponents << '\n';
+    for (int i = 0; i < graph.nrNodes; i++)
+        std::cout << i << ": " << graph.v[i]->lowLink << '\n';
+    free_graph(&graph);
+}
 
 void demo() {
     Graph graph;
     graph.nrNodes = 10;
-    graph.v = (Node**)malloc(graph.nrNodes * sizeof(Node*));
+    graph.v = (Node **) malloc(graph.nrNodes * sizeof(Node *));
     for (int i = 0; i < graph.nrNodes; ++i) {
-        graph.v[i] = (Node*)malloc(sizeof(Node));
+        graph.v[i] = (Node *) malloc(sizeof(Node));
         memset(graph.v[i], 0, sizeof(Node));
         graph.v[i]->key = i;
+        graph.v[i]->color = COLOR_WHITE;
+        graph.v[i]->f = 0;
+        graph.v[i]->d = 0;
+        graph.v[i]->parent = NULL;
     }
-    fa_graf_conex(&graph, 10);
+//    std::cout << "ASD";
+    fa_graf(&graph, 3);
     for (int i = 0; i < graph.nrNodes; i++) {
         std::cout << i << ": ";
         for (int j = 0; j < graph.v[i]->adjSize; j++)
@@ -260,16 +307,17 @@ void demo() {
             stiva.pop();
         }
     else
-        std::cout<<"ciclu\n";
+        std::cout << "ciclu\n";
     free_graph(&graph);
 }
 
 int main() {
     srand(time(nullptr));
     clock_t begin = clock();
-    demo();
-    // performance();
+//    demo();
+    ddemo();
+//     performance();
     clock_t end = clock();
-    printf("\n%lf\n", (double)(end - begin) / CLOCKS_PER_SEC);
+    printf("\n%lf\n", (double) (end - begin) / CLOCKS_PER_SEC);
     return 0;
 }
